2b66ccb92a751eaa4f22ff922c2eae48d845746c
kinironote <kinironote@gmail.com>
2016-12-19 17:26:53 +0900

    [clean]

--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@ require_once 'template/template.php';
 
 $tmpl_signupform = new Template('signup/form');
 $tmpl_signupform->redirect_url = 'signup.php';
-$tmpl_signupform->show('a');
+$tmpl_signupform->show();
 exit();
