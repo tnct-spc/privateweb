f7ef6e862319b8e0035b40621948ea60c5294e6a
kinironote <kinironote@gmail.com>
2016-12-21 15:53:21 +0900

    [update] サインインのパスワードをjsでハッシュに変更

--- web/template/signup/form.php
+++ web/template/signup/form.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
+		<script src="js/jsSHA/src/sha.js"></script>
 		<title>室内Web</title>
 	</head>
 	<body>
@@ -8,12 +10,22 @@
 		<?php echo isset($errormessage)? $errormessage.'<br>' : '' ?>
 		</font>
 		ユーザー登録フォーム<br>
-		<form action="<?php echo $redirect_url ?>" method="post">
+		<form id="signup" action="<?php echo $redirect_url ?>" method="post">
 			メールアドレス<input type="mail" name="mail" value="<?php echo $last_mail ?>" placeholder="サイボウズに登録されているアドレス"><br>
 			ユーザー名<input type="text" name="username" value="<?php echo $last_username ?>" placeholder="英数字3〜16文字"><br>
 			ニックネーム(4バイト文字未対応)<input type="text" name="nickname" value="<?php echo $last_nickname ?>" placeholder="1〜16文字"><br>
-			パスワード(<strong>サーバーに平文で送信されます</strong>)<input type="password" name="password" placeholder="英数字8〜16文字"><br>
-			<button>登録</button>
+			パスワード(<strong>サーバーに平文で送信されます</strong>)<input id="password" type="password" name="password" placeholder="英数字8〜16文字"><br>
+			<button type="submit">登録</button>
 		</form>
+		<script type="text/javascript" charset="utf-8">
+			$('#signup').submit(function(){
+				// パスワードをsha256でハッシュにする
+				var password = $('#password').val();
+				var sha = new jsSHA('SHA-256', 'TEXT');
+				sha.update(password);
+				var sha256_password = sha.getHash('HEX');
+				$('#password').val(sha256_password);
+			});
+		</script>
 	</body>
 </html>
